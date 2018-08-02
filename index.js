ca0a0c3bf5225bafdaad27d5dc770cf610a11dc4
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-08-02 23:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
-  return (`Welcome, Ada. You are number 1 in line.
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
